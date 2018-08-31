18bde4c12ce74af48a9c36ba9cbe8075ad0eed23
sami <sami.belbacha@vdm.fr>
2018-08-31 17:54:17 +0200

    Add Advertisement Form

--- src/Entity/Advertisement.php
+++ src/Entity/Advertisement.php
@@ -9,6 +9,24 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Advertisement
 {
+
+    CONST AIR_TRANSPORT_MODE      = 1 ;
+    CONST TRAIN_TRANSPORT_MODE    = 2 ;
+    CONST MARITIME_TRANSPORT_MODE = 3 ;
+    CONST ROAD_TRANSPORT_MODE     = 4;
+
+
+    CONST DOCUMENT_TYPE          = 1 ;
+    CONST CLOTHES_ACCESSORY_TYPE = 2 ;
+    CONST MEDICINE_TYPE          = 3 ;
+    CONST OTHER_TYPE             = 4 ;
+
+
+    CONST WEIGHT_LOWER_1  = 1 ;
+    CONST WEIGHT_1_3      = 2 ;
+    CONST WEIGHT_3_5      = 3 ;
+    CONST WEIGHT_HIGHER_5 = 4 ;
+
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
@@ -19,7 +37,22 @@ class Advertisement
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
      */
-    private $name;
+    private $title;
+
+    /**
+     * @ORM\Column(type="integer", length=255, nullable=true)
+     */
+    private $transportMode;
+
+    /**
+     * @ORM\Column(type="integer", length=255, nullable=true)
+     */
+    private $weight;
+
+    /**
+     * @ORM\Column(type="integer", length=255, nullable=true)
+     */
+    private $type;
 
     /**
      * @ORM\Column(type="string", length=255)
@@ -37,6 +70,11 @@ class Advertisement
     private $date;
 
     /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Description;
+
+    /**
      * @ORM\Column(type="float", nullable=true)
      */
     private $price;
@@ -46,17 +84,29 @@ class Advertisement
         return $this->id;
     }
 
-    public function getName(): ?string
+
+    /**
+     * @return mixed
+     */
+    public function getTitle(): ?string
     {
-        return $this->name;
+        return $this->title;
     }
 
-    public function setName(?string $name): self
+
+    /**
+     * @param string $title
+     *
+     * @return Advertisement
+     */
+    public function setTitle(string $title): self
     {
-        $this->name = $name;
+        $this->title = $title;
 
         return $this;
     }
+
+
 
     public function getFromCity(): ?string
     {
@@ -105,4 +155,93 @@ class Advertisement
 
         return $this;
     }
+
+
+    /**
+     * @return mixed
+     */
+    public function getTransportMode()
+    {
+        return $this->transportMode;
+    }
+
+
+    /**
+     * @param mixed $transportMode
+     *
+     * @return Advertisement
+     */
+    public function setTransportMode($transportMode)
+    {
+        $this->transportMode = $transportMode;
+
+        return $this;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getWeight()
+    {
+        return $this->weight;
+    }
+
+
+    /**
+     * @param mixed $weight
+     *
+     * @return Advertisement
+     */
+    public function setWeight($weight)
+    {
+        $this->weight = $weight;
+
+        return $this;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+
+    /**
+     * @param mixed $type
+     *
+     * @return Advertisement
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->Description;
+    }
+
+
+    /**
+     * @param mixed $Description
+     *
+     * @return Advertisement
+     */
+    public function setDescription($Description)
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+
 }
